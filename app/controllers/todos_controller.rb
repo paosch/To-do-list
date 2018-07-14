db981be89bcfa3423c76453cb40db8144de9cfe8
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2018-07-14 11:53:52 +0100

    The user can see their list of to-do items

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,14 @@
 class TodosController < ApplicationController
   def create
+    @todo = Todo.new(todo_params)
+    @todo.save
+    redirect_to root_path
   end
 
   def destroy
+  end
+
+  def todo_params
+    params.require(:todo).permit(:item)
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @todos = Todo.all
   end
 end
